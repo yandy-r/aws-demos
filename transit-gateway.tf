a68e52f27fed4939ceacbf0dab56bcd684d170fa
Yandy Ramirez <yandyr@gmail.com>
2019-05-18 20:20:43 +0000

    implement custom route tables
    This helps for hub-and-spoke model.

--- transit-gateway.tf
+++ transit-gateway.tf
@@ -7,8 +7,8 @@ module "transit_gateway" {
   transit_gateway_description     = "Transit Gateway Demo"
   amazon_side_asn                 = 65100
   auto_accept_shared_attachments  = "enable"
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
   dns_support                     = "enable"
   vpn_ecmp_support                = "enable"
 
@@ -20,8 +20,8 @@ module "transit_gateway" {
   vpc_ids                             = concat(local.core_vpc_ids, local.stub_vpc_ids)
   subnet_ids                          = [local.core_private_subnet_ids, local.stub1_subnet_ids, local.stub2_subnet_ids]
   ipv6_support                        = "disable"
-  associate_default_route_table       = true
-  vpc_default_route_table_propagation = true
+  associate_default_route_table       = false
+  vpc_default_route_table_propagation = false
   vpc_attachment_tags = [
     {
       Name = "Core VPC Attachment"
@@ -33,17 +33,38 @@ module "transit_gateway" {
       Name = "Stub-2 VPC Attachment"
     }
   ]
+
+  # stub route tables
+  create_custom_route_tables = true
+  route_table_count          = length(concat(local.core_vpc_ids, local.stub_vpc_ids))
+  route_table_tags = [
+    {
+      Name = "Core VPC Route Table"
+    },
+    {
+      Name = "Stub-1 VPC Route Table"
+    },
+    {
+      Name = "Stub-2 VPC Route Table"
+    }
+  ]
 }
 
-module "transit_gateway_default_route" {
-  source = "../modules/aws/transit-gateway-routing"
-
-  route_cidr_blocks = ["0.0.0.0/0"]
-  attachment_ids    = [module.transit_gateway.vpc_attachment_ids[0]]
-  route_table_ids   = [module.transit_gateway.default_route_table_id]
+# propagate from source VPC (attachments) to core route table
+resource "aws_ec2_transit_gateway_route_table_propagation" "stubs_to_core" {
+  count                          = 2
+  transit_gateway_attachment_id  = module.transit_gateway.vpc_attachment_ids[count.index + 1]
+  transit_gateway_route_table_id = module.transit_gateway.route_table_ids[0]
 }
 
-resource "aws_route" "core_routes" {
+resource "aws_ec2_transit_gateway_route" "default_routes" {
+  count                          = 3
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_attachment_id  = module.transit_gateway.vpc_attachment_ids[0]
+  transit_gateway_route_table_id = module.transit_gateway.route_table_ids[count.index]
+}
+
+resource "aws_route" "core_route_stubs" {
   count                  = length(local.core_route_table_ids)
   route_table_id         = local.core_route_table_ids[count.index]
   destination_cidr_block = "10.244.0.0/14"
