01395a6f80a8acfe7ddcf84a9a7c1b6166140f5d
Yandy Ramirez <yandyr@gmail.com>
2019-05-20 12:17:22 +0000

    cleanup locals file

--- locals.tf
+++ locals.tf
@@ -1,44 +1,52 @@
 locals {
+  core_vpc_ids              = [module.core_vpc.vpc_id]
+  spoke_vpc_ids             = [module.spoke_1_vpc.vpc_id, module.spoke_2_vpc.vpc_id]
+  spoke_1_subnet_ids        = module.spoke_1_vpc.private_subnets.*.id
+  spoke_2_subnet_ids        = module.spoke_2_vpc.private_subnets.*.id
+  core_public_subnet_ids    = module.core_vpc.public_subnets.*.id
+  core_private_subnet_ids   = module.core_vpc.private_subnets.*.id
+  core_pub_route_table_ids  = module.core_vpc.public_route_tables.*.id
+  core_priv_route_table_ids = module.core_vpc.private_route_tables.*.id
+
   vpc_ids = [
     module.core_vpc.vpc_id,
     module.spoke_1_vpc.vpc_id,
     module.spoke_2_vpc.vpc_id,
   ]
-  core_vpc_ids  = [module.core_vpc.vpc_id]
-  spoke_vpc_ids = [module.spoke_1_vpc.vpc_id, module.spoke_2_vpc.vpc_id]
+
   cidr_blocks = [
     module.core_vpc.cidr_block,
     module.spoke_1_vpc.cidr_block,
     module.spoke_2_vpc.cidr_block,
   ]
+
   spoke_subnet_ids = concat(
     module.spoke_1_vpc.private_subnets.*.id,
     module.spoke_2_vpc.private_subnets.*.id,
   )
-  spoke_1_subnet_ids = module.spoke_1_vpc.private_subnets.*.id
-  spoke_2_subnet_ids = module.spoke_2_vpc.private_subnets.*.id
+
   core_subnet_ids = concat(
     module.core_vpc.private_subnets.*.id,
     module.core_vpc.public_subnets.*.id,
   )
+
   subnet_ids = concat(
     module.core_vpc.private_subnets.*.id,
     module.core_vpc.public_subnets.*.id,
     module.spoke_1_vpc.private_subnets.*.id,
     module.spoke_2_vpc.private_subnets.*.id,
   )
-  core_public_subnet_ids    = module.core_vpc.public_subnets.*.id
-  core_private_subnet_ids   = module.core_vpc.private_subnets.*.id
-  core_pub_route_table_ids  = module.core_vpc.public_route_tables.*.id
-  core_priv_route_table_ids = module.core_vpc.private_route_tables.*.id
+
   core_route_table_ids = concat(
     module.core_vpc.public_route_tables.*.id,
     module.core_vpc.private_route_tables.*.id,
   )
+
   spoke_priv_route_table_ids = concat(
     module.spoke_1_vpc.private_route_tables.*.id,
     module.spoke_2_vpc.private_route_tables.*.id,
   )
+
   spoke_route_table_ids = concat(
     module.spoke_1_vpc.private_route_tables.*.id,
     module.spoke_2_vpc.private_route_tables.*.id,
