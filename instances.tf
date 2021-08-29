3171f5128f8665697e8e056cf0b8567d978da630
Yandy Ramirez <yandy@cloudart.io>
2021-08-29 19:32:48 -0400

    move ec2 data to same file as instances

--- instances.tf
+++ instances.tf
@@ -22,6 +22,54 @@ resource "aws_key_pair" "aws_test_key" {
   }
 }
 
+data "aws_ami" "amzn2_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-amd64-server*"]
+  }
+}
+
+data "template_file" "cloud_config" {
+  count    = length(var.hostnames)
+  template = file("${path.module}/cloud-config.tpl")
+
+  vars = {
+    hostname = var.hostnames[count.index]
+    ssh_key  = data.local_file.ssh_key.content
+  }
+}
+
+data "local_file" "ssh_key" {
+  filename = pathexpand(var.priv_ssh_key_path)
+
+  depends_on = [
+    aws_key_pair.aws_test_key
+  ]
+}
+
 resource "aws_network_interface" "central" {
   count             = 1
   subnet_id         = aws_subnet.public[0].id
