4d69d8aa4de0807c5856e02032174d4a144dff5c
Yandy Ramirez <yandy@cloudart.io>
2021-08-30 12:44:13 -0400

    add default provider for root module

--- provider.tf
+++ provider.tf
@@ -1,4 +1,19 @@
 provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = var.credentials_file
+  profile                 = var.aws_profile.sandbox
+
+  default_tags {
+    tags = {
+      Terraform   = "True"
+      Environemnt = "Test"
+      Owner       = "Dev"
+      Region      = "US-East-1"
+    }
+  }
+}
+
+provider "aws" {
   alias                   = "us_east_1"
   region                  = "us-east-1"
   shared_credentials_file = var.credentials_file
