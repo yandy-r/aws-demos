61cd30f30fa1d28b2dfacbb7f2451cc3627737e8
Yandy Ramirez <yandy@cloudart.io>
2021-08-30 23:22:26 -0400

    clean up and add some variables

--- main.tf
+++ main.tf
@@ -9,21 +9,8 @@ module "tgw_east" {
   bucket_name           = "east-3-${var.bucket_name}"
   region                = "us-east-1"
   create_peering_routes = true
-
-  vpc_cidr_blocks = [
-    "10.200.0.0/16",
-    "10.201.0.0/16",
-    "10.202.0.0/16",
-    "10.203.0.0/16"
-  ]
-
-  hostnames = [
-    "hub-e-bastion",
-    "hub-e-private",
-    "spoke-e-1",
-    "spoke-e-2",
-    "spoke-e-3"
-  ]
+  vpc_cidr_blocks       = var.east_vpc_cidrs
+  hostnames             = var.east_ec2_hostnames
 }
 
 module "tgw_west" {
@@ -37,24 +24,12 @@ module "tgw_west" {
   bucket_name           = "west2-3-${var.bucket_name}"
   region                = "us-west-2"
   create_peering_routes = true
-
-  vpc_cidr_blocks = [
-    "10.220.0.0/16",
-    "10.221.0.0/16",
-    "10.222.0.0/16",
-    "10.223.0.0/16"
-  ]
-
-  hostnames = [
-    "hub-w-bastion",
-    "hub-w-private",
-    "spoke-w-1",
-    "spoke-w-2",
-    "spoke-w-3"
-  ]
+  vpc_cidr_blocks       = var.west_vpc_cidrs
+  hostnames             = var.west_ec2_hostnames
 }
 
 locals {
+  east_tgw_vpc_ids     = [for v in module.tgw_east.vpcs : v.id]
   east_subnets         = module.tgw_east.subnets
   private_east_subnets = module.tgw_east.subnets.private
   public_east_subnets  = module.tgw_east.subnets.public
@@ -73,6 +48,7 @@ locals {
     module.tgw_east.tgw_rts[1]
   ]
 
+  west_tgw_vpc_ids     = [for v in module.tgw_west.vpcs : v.id]
   west_subnets         = module.tgw_west.subnets
   private_west_subnets = module.tgw_west.subnets.private
   public_west_subnets  = module.tgw_west.subnets.public
@@ -537,94 +513,3 @@ resource "aws_security_group_rule" "west_spoke_rules" {
   source_security_group_id = each.value.source_security_group_id
   security_group_id        = each.value.security_group_id
 }
-
-# output "public_east_ec2" {
-#   value = [for v in module.tgw_east.public_instances : {
-#     name       = v.tags_all.Name
-#     id         = v.id
-#     private_ip = v.private_ip
-#     public_ip  = v.public_ip
-#     } if v.public_ip != ""
-#   ]
-# }
-
-# output "private_east_ec2" {
-#   value = [for v in module.tgw_east.private_instances : {
-#     name       = v.tags_all.Name
-#     id         = v.id
-#     private_ip = v.private_ip
-#     }
-#   ]
-# }
-
-# output "public_west_ec2" {
-#   value = [for v in module.tgw_west.public_instances : {
-#     name       = v.tags_all.Name
-#     id         = v.id
-#     private_ip = v.private_ip
-#     public_ip  = v.public_ip
-#     } if v.public_ip != ""
-#   ]
-# }
-
-# output "private_west_ec2" {
-#   value = [for v in module.tgw_west.private_instances : {
-#     name       = v.tags_all.Name
-#     id         = v.id
-#     private_ip = v.private_ip
-#     }
-#   ]
-# }
-
-# output "vpc_east_info" {
-#   value = [
-#     for v in module.tgw_east.vpcs : {
-#       name       = v.tags_all.Name
-#       id         = v.id,
-#       cidr_block = v.cidr_block
-#     }
-#   ]
-# }
-
-
-
-
-# FOR LATER
-
-# output "east_subnets" {
-#   value = {
-#     for k, v in module.tgw_east.subnets : k => [
-#       for i in v : {
-#         name              = i.tags_all.Name
-#         id                = i.id
-#         vpc_id            = i.vpc_id
-#         cidr_block        = i.cidr_block
-#         availability_zone = i.availability_zone
-#       }
-#     ]
-#   }
-# }
-
-# output "private_east_subnets" {
-#   value = [
-#     for v in local.private_subnets : {
-#       name              = v.tags_all.Name
-#       id                = v.id
-#       vpc_id            = v.vpc_id
-#       cidr_block        = v.cidr_block
-#       availability_zone = v.availability_zone
-#     }
-#   ]
-# }
-
-# output "public_east_subnets" {
-#   value = [
-#     for v in local.public_subnets : {
-#       name              = v.tags_all.Name
-#       id                = v.id
-#       vpc_id            = v.vpc_id
-#       cidr_block        = v.cidr_block
-#       availability_zone = v.availability_zone
-#     }
-#   ]
-# }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,45 @@
-### --- EAST --- ###
+variable "east_vpc_cidrs" {
+  type = list(string)
+  default = [
+    "10.200.0.0/16",
+    "10.201.0.0/16",
+    "10.202.0.0/16",
+    "10.203.0.0/16"
+  ]
+}
+
+variable "east_ec2_hostnames" {
+  type = list(string)
+  default = [
+    "hub-e-bastion",
+    "hub-e-private",
+    "spoke-e-1",
+    "spoke-e-2",
+    "spoke-e-3"
+  ]
+}
+
+variable "west_ec2_hostnames" {
+  type = list(string)
+  default = [
+    "hub-w-bastion",
+    "hub-w-private",
+    "spoke-w-1",
+    "spoke-w-2",
+    "spoke-w-3"
+  ]
+}
+
+variable "west_vpc_cidrs" {
+  type = list(string)
+  default = [
+    "10.220.0.0/16",
+    "10.221.0.0/16",
+    "10.222.0.0/16",
+    "10.223.0.0/16"
+  ]
+}
+
 variable "self_public_ip" {
   type = string
 }
