7ca505a3cf4af4b5d8b7925df2d5bcd3be95e668
Yandy Ramirez <yandy@cloudart.io>
2021-09-01 22:09:39 -0400

    begin building ec2 module

---
+++ modules/ec2/main.tf
@@ -0,0 +1,60 @@
+### -------------------------------------------------------------------------------------------- ###
+### PROVIDERS
+### -------------------------------------------------------------------------------------------- ###
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.56"
+    }
+  }
+}
+
+data "aws_ami" "amzn2_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-amd64-server*"]
+  }
+}
+
+data "template_file" "cloud_config" {
+  count    = length(var.hostnames)
+  template = file("${path.module}/templates/cloud-config.tpl")
+
+  vars = {
+    hostname = var.hostnames[count.index]
+    ssh_key  = data.local_file.ssh_key.content
+  }
+}
+
+data "local_file" "ssh_key" {
+  filename = pathexpand("${var.priv_ssh_key_path}/${aws_key_pair.aws_test_key.key_name}")
+
+  depends_on = [
+    aws_key_pair.aws_test_key
+  ]
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -235,59 +235,6 @@ resource "aws_nat_gateway" "this" {
 # ### EC2 INSTANCES
 # ### -------------------------------------------------------------------------------------------- ###
 
-# resource "aws_key_pair" "aws_test_key" {
-#   key_name   = var.key_name
-#   public_key = var.priv_key.public_key_openssh
-# }
-
-# data "aws_ami" "amzn2_linux" {
-#   most_recent = true
-#   owners      = ["amazon"]
-
-#   filter {
-#     name   = "name"
-#     values = ["amzn2-ami-hvm*"]
-#   }
-
-#   filter {
-#     name   = "state"
-#     values = ["available"]
-#   }
-
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
-# }
-
-# data "aws_ami" "latest_ubuntu" {
-#   most_recent = true
-#   owners      = ["099720109477"]
-
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-amd64-server*"]
-#   }
-# }
-
-# data "template_file" "cloud_config" {
-#   count    = length(var.hostnames)
-#   template = file("${path.module}/templates/cloud-config.tpl")
-
-#   vars = {
-#     hostname = var.hostnames[count.index]
-#     ssh_key  = data.local_file.ssh_key.content
-#   }
-# }
-
-# data "local_file" "ssh_key" {
-#   filename = pathexpand("${var.priv_ssh_key_path}/${aws_key_pair.aws_test_key.key_name}")
-
-#   depends_on = [
-#     aws_key_pair.aws_test_key
-#   ]
-# }
-
 # resource "aws_network_interface" "hub" {
 #   count             = 1
 #   subnet_id         = aws_subnet.public[0].id
