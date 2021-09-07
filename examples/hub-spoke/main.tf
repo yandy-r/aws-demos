4650818d0c92aa99f7b7d6b42ae56baaf5fcd16f
Yandy Ramirez <yandy@cloudart.io>
2021-09-07 15:39:04 -0400

    update west vpn and static routes

--- examples/hub-spoke/main.tf
+++ examples/hub-spoke/main.tf
@@ -1750,3 +1750,113 @@ module "west_transit_gateway" {
     },
   ]
 }
+
+module "west_vpn" {
+  source    = "../../modules/vpc"
+  providers = { aws = aws.us_west_2 }
+  name      = "west_vpn"
+
+  customer_gateway = {
+    west_vpn = {
+      bgp_asn     = "65101"
+      device_name = "west_vpn"
+      ip_address  = var.lab_public_ip
+      type        = "ipsec.1"
+    }
+  }
+
+  vpn_connection = {
+    west_vpn = {
+      transit_gateway_id                   = module.west_transit_gateway.transit_gateway_id
+      static_routes_only                   = true
+      tunnel1_preshared_key                = var.tunnel1_preshared_key
+      tunnel2_preshared_key                = var.tunnel2_preshared_key
+      tunnel1_ike_versions                 = ["ikev2"]
+      tunnel2_ike_versions                 = ["ikev2"]
+      tunnel1_phase1_dh_group_numbers      = ["14"]
+      tunnel2_phase1_dh_group_numbers      = ["14"]
+      tunnel1_phase1_integrity_algorithms  = ["SHA1"]
+      tunnel2_phase1_integrity_algorithms  = ["SHA1"]
+      tunnel1_phase1_encryption_algorithms = ["AES128"]
+      tunnel2_phase1_encryption_algorithms = ["AES128"]
+      tunnel1_phase2_dh_group_numbers      = ["14"]
+      tunnel2_phase2_dh_group_numbers      = ["14"]
+      tunnel1_phase2_integrity_algorithms  = ["SHA1"]
+      tunnel2_phase2_integrity_algorithms  = ["SHA1"]
+      tunnel1_phase2_encryption_algorithms = ["AES128"]
+      tunnel2_phase2_encryption_algorithms = ["AES128"]
+    }
+  }
+
+  transit_gateway_route_tables = {
+    west_vpn = {
+      transit_gateway_id = module.west_transit_gateway.transit_gateway_id
+    }
+  }
+  transit_gateway_route_table_associations = {
+    west_vpn = {
+      transit_gateway_attachment_id  = module.west_vpn.vpn_transit_gateway_attachment_ids["west_vpn"]
+      transit_gateway_route_table_id = module.west_vpn.transit_gateway_route_table_ids["west_vpn"]
+    }
+  }
+  transit_gateway_route_table_propagations = {
+    hub_to_west_vpn = {
+      transit_gateway_attachment_id  = module.west_transit_gateway.vpc_attachment_ids["hub1"]
+      transit_gateway_route_table_id = module.west_vpn.transit_gateway_route_table_ids["west_vpn"]
+    }
+    spoke1_to_west_vpn = {
+      transit_gateway_attachment_id  = module.west_transit_gateway.vpc_attachment_ids["spoke1"]
+      transit_gateway_route_table_id = module.west_vpn.transit_gateway_route_table_ids["west_vpn"]
+    }
+    spoke2_to_west_vpn = {
+      transit_gateway_attachment_id  = module.west_transit_gateway.vpc_attachment_ids["spoke2"]
+      transit_gateway_route_table_id = module.west_vpn.transit_gateway_route_table_ids["west_vpn"]
+    }
+    spoke3_to_west_vpn = {
+      transit_gateway_attachment_id  = module.west_transit_gateway.vpc_attachment_ids["spoke3"]
+      transit_gateway_route_table_id = module.west_vpn.transit_gateway_route_table_ids["west_vpn"]
+    }
+  }
+  vpn_transit_gateway_routes = [
+    {
+      destination                    = var.lab_local_cidr
+      transit_gateway_attachment_id  = module.west_vpn.vpn_transit_gateway_attachment_ids["west_vpn"]
+      transit_gateway_route_table_id = module.west_transit_gateway.route_table_ids["hubs"]
+    },
+    {
+      destination                    = var.lab_local_cidr
+      transit_gateway_attachment_id  = module.west_vpn.vpn_transit_gateway_attachment_ids["west_vpn"]
+      transit_gateway_route_table_id = module.west_transit_gateway.route_table_ids["spokes"]
+    }
+  ]
+
+  routes = [
+    {
+      destination_cidr_block = var.lab_local_cidr
+      route_table_id         = module.west_hub.public_route_table_id
+      transit_gateway_id     = module.west_transit_gateway.transit_gateway_id
+    },
+    {
+      destination_cidr_block = var.lab_local_cidr
+      route_table_id         = module.west_hub.private_route_table_id
+      transit_gateway_id     = module.west_transit_gateway.transit_gateway_id
+    },
+    {
+      destination_cidr_block = var.lab_local_cidr
+      route_table_id         = module.west_hub.intra_route_table_id
+      transit_gateway_id     = module.west_transit_gateway.transit_gateway_id
+    }
+  ]
+
+  security_group_rules = [
+    {
+      description       = "Allow ALL sourced from home lab"
+      type              = "ingress"
+      from_port         = 0
+      to_port           = 0
+      protocol          = "-1"
+      cidr_blocks       = [cidrsubnet(var.lab_local_cidr, 8, 0)]
+      security_group_id = module.west_hub.security_group_ids["public1"]
+    },
+  ]
+}
