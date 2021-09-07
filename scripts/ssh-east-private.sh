82d6ae76351423498213625692b1e5644201fc8c
Yandy Ramirez <yandy@cloudart.io>
2021-09-07 15:13:14 -0400

    add scripts to auto ssh to private ips

---
+++ scripts/ssh-east-private.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+EAST_BASTION="$(jq -r '.resources[] |
+select(.name == "this") |
+select(.type == "aws_instance") |
+select(.module == "module.east_ec2") |
+.instances[0].attributes.private_ip' <terraform.tfstate)"
+
+ssh -o "StrictHostKeyChecking no" \
+  -o "UserKnownHostsFile /dev/null" \
+  -i "${HOME}"/.aws-keys/aws-test-key \
+  ec2-user@"${EAST_BASTION}"
