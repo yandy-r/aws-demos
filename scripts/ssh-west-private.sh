6058fe855e8eb3d6c61b221adbc75f0684ccf3e8
Yandy Ramirez <yandy@cloudart.io>
2021-09-10 13:03:46 -0400

    delete files

--- scripts/ssh-west-private.sh
+++ scripts/ssh-west-private.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-EAST_BASTION="$(jq -r '.resources[] |
+WEST_BASTION="$(jq -r '.resources[] |
 select(.name == "this") |
 select(.type == "aws_instance") |
 select(.module == "module.west_ec2") |
 .instances[0].attributes.private_ip' <terraform.tfstate)"
 
+echo "Remoting into ${WEST_BASTION}"
 ssh -o "StrictHostKeyChecking no" \
   -o "UserKnownHostsFile /dev/null" \
   -i ${HOME}/.aws-keys/aws-test-key \
-  ec2-user@"${EAST_BASTION}"
+  cloud-useer@"${WEST_BASTION}"
